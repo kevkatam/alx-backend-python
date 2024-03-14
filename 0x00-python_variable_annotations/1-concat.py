5ac034ccb92381ae043cb5f6122737c9a00bf31f
kevkatam <kevkatam@gmail.com>
2024-03-14 15:53:19 +0300

    adding 2-floor.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+module containing a function that takes a string str1 and a string str2 as
+arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function that concanates two strings """
+    return str1 + str2
